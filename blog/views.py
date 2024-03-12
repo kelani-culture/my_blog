b25c3c17fcd277def73200def55da1b90c64761d
Null <kelanidarasimi9@gmail.com>
2024-03-12 09:44:42 +0100

    learned how to create custom template tags

--- blog/views.py
+++ blog/views.py
@@ -4,34 +4,42 @@ from django.http import Http404
 from django.core.paginator import (Paginator, EmptyPage,
                                    PageNotAnInteger)
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
-class PostListView(ListView):
-    """
-    Alternative post list view
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     """
+#     Alternative post list view
+#     """
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
-# Create your views here.
-# def post_list(request):
-#     post = Post.published.all()
-#     paginator = Paginator(post, 3)
-#     page_number = request.GET.get('page', 1)
-
-#     try:
-#         posts = paginator.page(page_number)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'posts': posts})
+#Create your views here.
+def post_list(request, tag_slug=None):
+    post = Post.published.all()
+    
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -41,9 +49,22 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day) 
+    # Lists of active comment for this post
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).\
+                    exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).\
+                    order_by('-same_tags', '-publish')
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -76,3 +97,25 @@ def post_share(request, post_id):
                    {'post': post,
                     'form': form,
                     'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # create a comment object without saving it to the Database
+        comment = form.save(commit=False)
+        # Assign the post ot the comment
+        comment.post = post
+        # save the comment to the database
+        comment.save()
+        
+    
+    return render(request,
+                  'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
